e3528d69ab61ee33ad5d4d6ff0bac49de1ebc493
Vadim Manakov <manakov_vadim2004@mail.ru>
2024-11-27 16:09:08 +0300

    e

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -56,7 +56,8 @@ def post_detail(request, id):
     return render(request, 'blog/detail.html', {'post': post})
 
 
-
 def category_posts(request, category_slug):
     filtered_posts = [post for post in posts if post["category"] == category_slug]
-    return render(request, 'blog/category.html', {'category': category_slug, 'posts': filtered_posts})
+    return render(request, 'blog/category.html',
+                  {'category': category_slug,
+                   'posts': filtered_posts})
