2766a44870b1ebfc98ac24d684350c10414cdc39
Vadim Manakov <manakov_vadim2004@mail.ru>
2024-11-27 16:11:06 +0300

    e

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -46,14 +46,16 @@ posts = [
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'posts': reversed(posts)})
+    return render(request, 'blog/index.html',
+                  {'posts': reversed(posts)})
 
 
 def post_detail(request, id):
     post = next((post for post in posts if post['id'] == id), None)
     if post is None:
-        return render(request, '404.html', status=404)  # Страница не найдена
-    return render(request, 'blog/detail.html', {'post': post})
+        return render(request, '404.html', status=404)
+    return render(request, 'blog/detail.html',
+                  {'post': post})
 
 
 def category_posts(request, category_slug):
